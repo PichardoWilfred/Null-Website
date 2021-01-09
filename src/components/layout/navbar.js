907f0296219a9b51716b52c7ac0b4419a94233f0
Wilfred <pichardowilfred@gmail.com>
2021-01-09 16:52:25 -0400

    Navbar v2

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,12 +1,28 @@
-import React, { useState } from "react"
-import styled from "styled-components"
+import React, { useState, useEffect } from "react"
+import styled, { css } from "styled-components"
 import { device } from "./responsive/device"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
 export default function Navbar() {
-  const [show, setShow] = useState(false)
-  //Query logo
+  const [showNav, setShowNav] = useState(false) //toggle mobile navbar
+  const [headerShow, setHeaderShow] = useState(true) //hide navbar on scroll
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+
+  useEffect(() => {
+    function handleScroll() {
+      const currentScrollPos = window.pageYOffset
+      if (!showNav)
+        setHeaderShow(prevScrollPos > currentScrollPos || currentScrollPos < 50)
+
+      setPrevScrollPos(currentScrollPos)
+    }
+    window.addEventListener("scroll", handleScroll) //Listen to scroll
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [showNav, prevScrollPos, headerShow])
+
   const data = useStaticQuery(graphql`
     query {
       image: file(relativePath: { eq: "logo.png" }) {
@@ -17,26 +33,34 @@ export default function Navbar() {
         }
       }
     }
-  `)
-  //
+  `) //Query logo
   return (
-    <Header>
-      <Img className="logo" fixed={data.image.childImageSharp.fixed} />
-      <ToggleMenu onClick={() => setShow(!show)}>
+    <Header showThis={headerShow}>
+      <Img
+        css={`
+          margin-left: 1.3rem;
+          @media ${device.tablet} {
+            width: 2.2rem !important;
+            height: 2.2rem !important;
+            margin-top: 0.3rem;
+          }
+        `}
+        fixed={data.image.childImageSharp.fixed}
+      />
+      <ToggleMenu onClick={() => setShowNav(!showNav)}>
         <div></div>
         <div></div>
         <div></div>
       </ToggleMenu>
 
-      <Navigation display={show} className={show ? "active" : "hidden"}>
+      <Navigation showThis={showNav}>
         <NavLinks>
           <Link>
-            <a href="#">Servicios</a>
+            <A>Servicios</A>
           </Link>
           <Link>
-            <a href="#">Nosotros</a>
+            <A>Nosotros</A>
           </Link>
-          {/* <!-- Also they go with a underline animation --> */}
           <ContactButton>CONTÁCTANOS</ContactButton>
         </NavLinks>
       </Navigation>
@@ -44,11 +68,22 @@ export default function Navbar() {
   )
 }
 
+const hiddenHeader = css`
+  visibility: hidden;
+  transition: all 200ms ease-out;
+  transform: translate(0, -100%);
+`
+const visibleHeader = css`
+  visibility: visible;
+  transition: all 200ms ease-in;
+`
+
 const Header = styled.header`
   position: fixed;
   top: 0;
 
   display: flex;
+  ${props => (props.showThis ? visibleHeader : hiddenHeader)}
   flex-wrap: wrap;
   justify-content: space-between;
 
@@ -57,27 +92,9 @@ const Header = styled.header`
 
   padding: 0.8rem 0;
   background-color: black;
-  .logo {
-    margin-left: 1.3rem;
-  }
+
   @media ${device.tablet} {
-    padding-top: 0.7rem;
-    .logo {
-      width: 2.2rem !important;
-      height: 2.2rem !important;
-      margin-top: 0.3rem;
-    }
-    //Toggle menu animation
-    .hidden {
-      transition: all 100ms cubic-bezier(0.65, -1.26, 0.97, 1.06);
-      transform-origin: top;
-      transform: scaleY(0);
-    }
-    .active {
-      transition: all 100ms cubic-bezier(0.45, 0.84, 0.71, 1);
-      transform-origin: top;
-      transform: scaleY(1);
-    }
+    padding-top: 0.5rem;
   }
 `
 const ToggleMenu = styled.div`
@@ -96,15 +113,26 @@ const ToggleMenu = styled.div`
     border-radius: 0.3rem;
     padding: 0.3rem 0.8rem;
     margin-right: 0.8rem;
-  } //El espacio total a la derecha debe de ser 1.3rem
+  } //El espacio total a la derecha debe de ser 1.1rem
 `
 const Navigation = styled.nav`
   padding-right: 0.5rem;
   @media ${device.tablet} {
-    margin-top: 0.54rem;
+    ${props => (props.showThis ? visibleNav : hiddenNav)}
+    margin-top: 0.74rem;
     width: 100%;
     padding-right: 0;
   }
+` //Hide Navigation bar
+const hiddenNav = css`
+  transition: all 100ms cubic-bezier(0.65, -1.26, 0.97, 1.06);
+  transform-origin: top;
+  transform: scaleY(0);
+` //Show Navigation bar
+const visibleNav = css`
+  transition: all 100ms cubic-bezier(0.45, 0.84, 0.71, 1);
+  transform-origin: top;
+  transform: scaleY(1);
 `
 const NavLinks = styled.ul`
   @media ${device.tablet} {
@@ -116,17 +144,24 @@ const Link = styled.li`
   padding: 0 0.5rem;
   font-weight: 400;
 
-  a {
-    text-decoration: none;
-    color: white;
+  @media ${device.tablet} {
+    padding: 1.3rem;
+    background-color: black;
+    width: 100%;
+    border-top: 1px solid white;
   }
+`
+const A = styled.a`
+  text-decoration: none;
+  color: white;
+
   &:active {
     transition: all 0.2s;
     background-color: #222222;
   }
   //feature this only on Desktop
   @media (min-width: 768px) {
-    a::after {
+    &::after {
       content: " ";
       display: block;
       width: 100%; //Overflows on the left if bigger
@@ -136,18 +171,13 @@ const Link = styled.li`
       transition: all 0.15s;
       transform: scaleX(0);
     }
-    a:hover::after {
+    &:hover::after {
       transform-origin: center;
       transform: scaleX(1);
     }
   }
-  @media ${device.tablet} {
-    padding: 1.3rem;
-    background-color: black;
-    width: 100%;
-    border-top: 1px solid white;
-  }
 `
+
 const ContactButton = styled.button`
   -webkit-box-shadow: none;
   box-shadow: none;
@@ -182,7 +212,7 @@ const ContactButton = styled.button`
     padding: 1.3rem;
     border-radius: 0;
     width: 100%;
-    border-bottom: 1.5px solid black;
+    border-bottom: 1.9px solid black;
     &:active {
       transition: all 0.2s;
       background-color: #eaecee;
